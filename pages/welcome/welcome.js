e046c1df83e373d80e8b600f34b48f582eee96ab
Shinan Xu <shinan.xu97@gmail.com>
2020-03-01 03:50:40 +0800

    v0.0.1e add wux components

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,7 +1,8 @@
 //welcome.js
 Page({
   data: {
-    numPeople: 3
+    numPeople: 3,
+    currentStep: 0
   },
   addPeople: function (e) {
     const numPeople = this.data.numPeople < 9 ? this.data.numPeople + 1 : this.data.numPeople;
@@ -16,6 +17,13 @@ Page({
       numPeople,
     })
     console.log('-1 people. current people: ', numPeople);
+  },
+  nextStep() {
+    const currentStep = this.data.currentStep + 1 > 3 ? 3 : this.data.currentStep + 1
+    this.setData({
+      currentStep,
+    })
+    console.log('next step. current step: ', currentStep);
   },
   test: function (e) {
     console.log('test');
